d1108306148550d646ebd08ffdfc7fa9e71c0076
Luiz Felipe <aguiarpassaroni@gmail.com>
2026-03-25 12:14:04 -0300

    Initial Commit

---
+++ Tecnoblog/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  Tecnoblog
+//
+//  Created by LUIZ PASSARONI on 25/03/26.
+//  Copyright © 2026 Globo Comunicação e Participações S.A.  All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Tecnoblog/TecnoblogApp.swift
@@ -0,0 +1,18 @@
+//
+//  TecnoblogApp.swift
+//  Tecnoblog
+//
+//  Created by LUIZ PASSARONI on 25/03/26.
+//  Copyright © 2026 Globo Comunicação e Participações S.A.  All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct TecnoblogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
